697c03ea187f746a7bbe897f0f87c176f5646e33
D3atHpR <kastiel103@gmail.com>
2021-02-04 22:59:55 -0300

    Adicionar arquivos de projeto.

---
+++ XGame.Domain/Entities/MyJogos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace XGame.Domain.Entities
+{
+    public class MyJogos
+    {
+        public Guid Id { get; set; }
+
+        public JogoPlataforma JogoPlataforma { get; set; }
+
+        public bool Deseja { get; set; }
+
+        public bool Troca { get; set; }
+
+        public bool Venda { get; set; }
+
+        public DateTime DataDesejo { get; set; }
+    }
+
+}
---
+++ XGame.Domain/Entities/Plataforma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace XGame.Domain.Entities
+{
+     public class Plataforma
+    {
+        public Guid Id { get; set; }
+
+        public string Nome { get; set; }
+
+
+
+
+
+    }
+}
